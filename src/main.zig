a5e34bf97a1c36f9a3c7a5f32c5a5296a564ec64
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2023-07-28 12:58:14 +0200

    Initial commit

---
+++ src/main.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    //c.InitWindow(800, 450, "raylib [core] example - basic window");
+
+}
